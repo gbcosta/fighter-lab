6c9699af63486ef6e321d265dc773c3c12025a83
Gabriel <gbcostasant@gmail.com>
2026-05-02 22:23:24 -0300

    menu update

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -2,20 +2,41 @@ const Button = ({ children }: { children: string }) => {
   return (
     <li
       className="p-2 flex items-center text-center bg-red-600 rounded-md text-sm
-            font-bold"
+            font-bold cursor-pointer"
     >
       {children}
     </li>
   );
 };
+
 export const Menu = () => {
   return (
-    <nav className="h-full px-2 py-4">
-      <ul className="flex flex-col gap-2">
-        <Button>3S</Button>
-        <Button>2X</Button>
-        <Button>+R</Button>
-      </ul>
+    <nav className="h-screen flex">
+      <div className="border-r border-r-white/10 h-full">
+        <ul className="flex flex-col gap-3 mt-16 px-2">
+          <Button>3S</Button>
+          <Button>2X</Button>
+          <Button>+R</Button>
+        </ul>
+      </div>
+      <div className="border-r border-r-white/10 h-full">
+        <div className="px-8 pt-8">
+          <div className="flex gap-2">
+            <div
+              className="h-10 w-10 flex items-center justify-center font-black
+                            bg-red-600 rounded-sm"
+            >
+              3S
+            </div>
+            <span
+              className="uppercase font-semibold text-yellow-400 hover:text-white 
+                            cursor-pointer duration-150"
+            >
+              Third Strike
+            </span>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Menu } from "./components/menu";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const interSans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
   subsets: ["latin"],
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${interSans.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex">
         <Menu />
